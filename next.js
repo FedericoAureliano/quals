44af0ad40dd152edbdd5235046fdc6d2ae9355ee
FedericoAureliano <fmorarocha@gmail.com>
2023-01-31 17:37:59 -0800

    proof of concept

--- next.js
+++ next.js
@@ -1,9 +1,16 @@
-const NUMBER_OF_SLIDES = 2;
+const NUMBER_OF_SLIDES = 3;
 
 var slide = parseInt(document.URL.split('/').pop().split('.')[0], 10);
 
-const footer = document.getElementById('foot');
-footer.innerText = "Slide " + slide;
+const footer = document.getElementById('slide-number');
+footer.innerText = slide + " of " + NUMBER_OF_SLIDES
+
+const date = document.getElementById('date');
+if (date != null) {
+  const currentDate = new Date();
+  const options = { year: 'numeric', month: 'short', day: 'numeric' };
+  date.innerText = currentDate.toLocaleDateString('en-us', options);
+}
 
 const body = document.querySelector('body');
 body.onkeydown = function(e){step(e)};
@@ -11,9 +18,9 @@ body.onkeydown = function(e){step(e)};
 function step(e) {
 
   if (e.code == "KeyD") {
-    slide = Math.min(NUMBER_OF_SLIDES - 1, slide + 1);
+    slide = Math.min(NUMBER_OF_SLIDES, slide + 1);
   } else if (e.code == "KeyA") {
-    slide = Math.max(0, slide - 1)
+    slide = Math.max(1, slide - 1)
   }
 
   const new_slide = document.URL.split('/').slice(0, -1).join('/') + '/' + slide + '.html';
