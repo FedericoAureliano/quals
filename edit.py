bd36a3a2433452819818c311f4a5f12da8734b06
FedericoAureliano <fmorarocha@gmail.com>
2023-02-14 18:25:13 -0800

    support deleting too

--- edit.py
+++ edit.py
@@ -3,23 +3,6 @@
 import sys
 import os
 
-i = int(sys.argv[1])
-
-# get all files in the slides directory
-files = [f for f in os.listdir('slides') if os.path.isfile(os.path.join('slides', f))]
-# filter out the files that are not .html files and whose name is not a number
-files = [f for f in files if f.endswith('.html') and f[:-5].isnumeric()]
-
-# sort the files by their number
-files.sort(key=lambda f: int(f[:-5]))
-
-# get the files that are after the file to be inserted
-files_after = files[i-1:]
-
-# shift the files that are after up by one number in reverse order
-for f in reversed(files_after):
-    os.rename(os.path.join('slides', f), os.path.join('slides', str(int(f[:-5]) + 1) + '.html'))
-
 page = """<!DOCTYPE html>
 <html lang="en">
 
@@ -62,14 +45,52 @@ page = """<!DOCTYPE html>
 </html>
 """
 
-# create the new file
-with open(os.path.join('slides', str(i) + '.html'), 'w') as f:
-    f.write(page)
+input = int(sys.argv[1])
+
+# get all files in the slides directory
+files = [f for f in os.listdir('slides') if os.path.isfile(os.path.join('slides', f))]
+# filter out the files that are not .html files and whose name is not a number
+files = [f for f in files if f.endswith('.html') and f[:-5].isnumeric()]
+
+# sort the files by their number
+files.sort(key=lambda f: int(f[:-5]))
+
+def insert(i):
+  # get the file currently in that place and all the files after it
+  files_after = files[i-1:]
+
+  # shift the files that are after up by one number in reverse order
+  for f in reversed(files_after):
+      os.rename(os.path.join('slides', f), os.path.join('slides', str(int(f[:-5]) + 1) + '.html'))
+
+  # create the new file
+  with open(os.path.join('slides', str(i) + '.html'), 'w') as f:
+      f.write(page)
+
+  return len(files) + 1
+
+def delete(i):
+    # get all the files after the one we want to delete
+    files_after = files[i:]
+
+    # delete the file
+    os.remove(os.path.join('slides', str(i) + '.html'))
+
+    # shift the files that are after down by one number
+    for f in files_after:
+        os.rename(os.path.join('slides', f), os.path.join('slides', str(int(f[:-5]) - 1) + '.html'))
+    
+    return len(files) - 1
+
+if input > 0:
+    new_count = insert(input)
+else:
+    new_count = delete(-input)
 
 # open next.js and replace the first line with "const NUMBER_OF_SLIDES = k;" where k is the number of slides
 with open('next.js', 'r') as f:
     lines = f.readlines()
-    lines[0] = f"const NUMBER_OF_SLIDES = {len(files) + 1};\n"
+    lines[0] = f"const NUMBER_OF_SLIDES = {new_count};\n"
 
 with open('next.js', 'w') as f:
     f.writelines(lines)
